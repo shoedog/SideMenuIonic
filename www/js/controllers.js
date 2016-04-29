5e1ee2d251a826b17e6e7d5fe75ca93e28435aa8
shoedog <wesley.jinks@gmail.com>
2016-04-29 07:50:10 -0600

    Updated Controller & app.js to render view of sites

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -41,15 +41,12 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+.controller('SitesCtrl', function($scope, $http) {
+  $scope.sites = [ ];
+  $http.get('/sites.json')
+    .then(function (response) {
+    $scope.sites = response.data;
+    });
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
